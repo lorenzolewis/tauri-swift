6217f2f408aba52c226d57551add3adcf3ca5239
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-07-17 22:13:27 +0100

    Add swift linker

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
+use swift_rs::build;
+
 fn main() {
-  tauri_build::build()
+    build::link_swift();
+    build::link_swift_package("SwiftLibrary", "../SwiftLibrary");
+    tauri_build::build();
 }
